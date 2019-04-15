0d2e6cb199a6563dafba3b3d0d2b2ec6fc756d10
Michelle Mei-Ling Sandford <msandford@outlook.com>
2019-04-15 08:55:58 +0800

    Update quickstart.js
    domain

--- public/quickstart.js
+++ public/quickstart.js
@@ -8,7 +8,7 @@
   var device;
 
   log('Requesting Capability Token...');
-  $.getJSON('https://YOUR_FUNCTION_SUBDOMAIN_HERE.twil.io/capability-token')
+  $.getJSON('https://sand-caiman-4306.twil.io/capability-token')
     .then(function (data) {
       log('Got a token.');
       console.log('Token: ' + data.token);
